c1fa796703fa06075b314542177382197a97c337
李* <jiandinglixiang@qq.com>
2025-02-21 16:52:02 +0800

    build: 更新 ESLint 和 Prettier 配置
    - 移除旧的 .eslintrc 和 .prettierrc 文件
    - 添加新的 ESLint 配置文件 eslint.config.js
    - 更新 ESLint 规则以支持 TypeScript 和 React
    - 配置 Prettier 格式化规则

---
+++ prettier.config.js
@@ -0,0 +1,11 @@
+export default {
+  semi: false,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'es5',
+  printWidth: 80,
+  bracketSpacing: true,
+  arrowParens: 'avoid',
+  plugins: ['prettier-plugin-organize-imports'],
+  organizeImportsSkipDestructiveCodeActions: true,
+}
